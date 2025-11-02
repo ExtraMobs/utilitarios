f8763f0e9b8f1981a4c337c0e619509f75fcc5ce
Mario Cesar Silva Elvira <mariocesarelvira@hotmail.com>
2025-11-02 16:04:57 -0300

    refactor:  nova ferramenta de backup

---
+++ lib/ui/backup/widgets/backup_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BackupScreen extends StatelessWidget {
+  const BackupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
